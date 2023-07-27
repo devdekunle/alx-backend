4b344647cd45e7b679bb2b9352027fd499120425
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-27 21:12:42 +0100

    implement Least Recently Used(LRU) cache replacement policy

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+a class BasicCache that inherits from BaseCaching
+and is a caching system:
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Basic caching system implementing the LRU(Least recently Used)
+    replacement policy
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.order_list = list()
+        self.put_count = 0
+
+    def put(self, key, item):
+        """
+        assign the value of a key to the cache_data attribute if the
+        parent class
+        """
+        self.cache_data[key] = item
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            key_to_del = self.order_list[self.put_count]
+            print("DISCARD: {}".format(key_to_del))
+            self.put_count += 1
+            del self.cache_data[key_to_del]
+            if key in self.order_list:
+                self.order_list.remove(key)
+            self.order_list.append(key)
+        if key not in self.order_list:
+            self.order_list.append(key)
+        else:
+            self.order_list.remove(key)
+            self.order_list.append(key)
+
+    def get(self, key):
+        "get the value for a key from a cache"
+        if key and key in self.cache_data.keys():
+            self.order_list.remove(key)
+            self.order_list.append(key)
+            return self.cache_data[key]
+        else:
+            return None
