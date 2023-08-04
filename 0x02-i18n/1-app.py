d302ef9fd40777fc872cc0f9ab604372be98f100
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-04 15:36:26 +0100

    implement basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+creating a flask app instance
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """
+    class to configure Babel for i18n
+    """
+    LANGUAGE = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+@app.route('/', methods=['GET'])
+def index_route():
+    """
+    an index page for testing i18n
+    """
+    return render_template('1-index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=1)
