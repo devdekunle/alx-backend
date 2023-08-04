2615f0034b6038a7782b1e5cdac6b3b80ed91fd4
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-04 14:48:19 +0100

    fix pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,13 +6,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index_route():
     """
     an index page for testing i18n
     """
     return render_template('index.html')
-
 
 
 if __name__ == "__main__":
